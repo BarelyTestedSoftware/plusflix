7953421ef7c3f65b41283a17472f82ceff368a9f
Kasia Pstrokońska <kasiapstrokonska03@gmail.com>
2026-01-08 12:00:43 +0100

    fix movie styling

--- components/movie-list/movie/index.php
+++ components/movie-list/movie/index.php
@@ -8,12 +8,13 @@ $show = $params['show'];
 <div class="movie" style="background-image: url('<?php echo $show->coverImage->src; ?>');">
     <button class="movie-like">♡</button>
     <div class="movie-info">
-        <h3 class="movie-title"><?php echo htmlspecialchars($show->title); ?></h3>
         <div class="movie-rating">
             <span class="star">★</span>
-            <span class="rating-value"><?php echo number_format($show->ratings[0]->value / 20, 1); ?></span>
-            <span class="rating-count">(<?php echo count($show->ratings); ?>)</span>
+            <span class="rating-value"><?= number_format($show->ratings[0]->value / 20, 1); ?></span>
+            <span class="rating-count">(<?= count($show->ratings); ?>)</span>
         </div>
+        <h3 class="movie-title"><?= e($show->title); ?></h3>
+
         <div class="movie-meta">
             <span class="meta-item"><i class="fas fa-calendar"></i> <?php echo substr($show->productionDate, 0, 4); ?></span>
             <span class="meta-item"><i class="fas fa-tv"></i> <?php echo $show->numberOfEpisodes; ?> ser.</span>
